d2ae4de593ebcd3210671dab692f2cf17e4cabea
Geert Geerits <ggeerits@hotmail.com>
2022-08-16 16:36:10 +0200

    Add project files.

---
+++ MauiAppTestDisplayAlert/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppTestDisplayAlert;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppTestDisplayAlert/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiAppTestDisplayAlert;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        ShowLicense();
+    }
+
+    private async void ShowLicense()
+    {
+        bool bAnswer = await Application.Current.MainPage.DisplayAlert("License", "Blablabla", "Yes", "No");
+
+        //if (bAnswer)
+        //{
+        //    Preferences.Default.Set("SettingLicense", true);
+        //}
+        //else
+        //{
+        //    Application.Current.Quit();
+        //}
+    }
+}
+
